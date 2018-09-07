896c76b8f6d1d0de018f3c155c666aaed8d213e6
daekeun-kim <daekeun.kim@kla-tencor.com>
2018-09-07 14:11:06 +0900

    first

---
+++ src/webparts/intelTest/components/IIntelTestProps.ts
@@ -0,0 +1,3 @@
+export interface IIntelTestProps {
+  description: string;
+}
---
+++ src/webparts/intelTest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IIntelTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'IntelTestWebPartStrings' {
+  const strings: IIntelTestWebPartStrings;
+  export = strings;
+}
